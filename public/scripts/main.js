46d211686ac492aad2e61ef1c52b0e7dac54b196
Vinci <vincicat@gmail.com>
2014-05-14 23:51:39 +0800

    (Public) Added the contact form with script.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('Allo Allo!');
+$(".contact.page .contact-form").on("submit", function(e) {
+  var entities, entry, obj, payload, _i, _len;
+  e.preventDefault();
+  entities = $(e.target).serializeArray();
+  obj = {};
+  for (_i = 0, _len = entities.length; _i < _len; _i++) {
+    entry = entities[_i];
+    obj[entry["name"]] = entry["value"];
+  }
+  obj["name"] = obj["firstName"] + " " + obj["lastName"];
+  payload = JSON.stringify(obj);
+  $.ajax({
+    url: 'https://looppulse-test.firebaseio.com/.json',
+    type: 'POST',
+    data: payload,
+    dataType: "json",
+    success: function(data) {
+      if (data.name) {
+        return alert("Success");
+      }
+    }
+  });
+  return false;
+});
